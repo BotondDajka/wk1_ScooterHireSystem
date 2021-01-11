b120bdf96f06702d5bf762d8bc2aaa4d33af8c98
csontvazharcos <skeletonfighter@gmail.com>
2021-01-11 10:00:27 +0000

    Family tree with childOf method

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello")
+let john = {
+    name: "John Doe",
+    parents: [],
+    childOf: function() {
+        let parentsString = "";
+        for (const parent in this.parents){
+            parentsString += this.parents[parent].name + " " ;
+        }
+        return parentsString
+    },
+}
+
+
+let johnJr = {
+    name: "John Doe Jr",
+    parents: [john],
+    childOf: function() {
+        let parentsString = "";
+        for (const parent in this.parents){
+            parentsString += this.parents[parent].name + " " ;
+        }
+        return parentsString
+    },
+}
+
+let johnJrJr = {
+    name: "John Doe Jr Jr",
+    parents: [johnJr],
+    childOf: function() {
+        let parentsString = "";
+        for (const parent in this.parents){
+            parentsString += this.parents[parent].name + " " ;
+        }
+        return parentsString
+    },
+}
+
+
+console.log(john.childOf())
+
+
