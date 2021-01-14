730f04f7e68f69ea875b355794f7542a60e8d4ff
csontvazharcos <skeletonfighter@gmail.com>
2021-01-14 19:22:16 +0000

    ScooterHire class added

--- classes/h_ScooterHire.js
+++ classes/h_ScooterHire.js
@@ -1,42 +1,46 @@
+
 /**
  * @class Parent Object of the Scooter Hire System
+ * @property {Array<Scooter>} allScooters List of every scooter
+ * @property {Array<Scooter>} availableScooters List of available scooters
+ * @property {Array<ChargeStation>} chargingStations List of Charging Stations
+ * @property {Array<User>} userList List of registered users
  */
-class ScooterHire {
-    /**@property {Array<Scooter>} allScooters List of every scooter*/
+export class ScooterHire {
     static allScooters = [];
-    /**@property {Array<Scooter>} availableScooters List of available scooters*/
     static availableScooters = [];
 
-    /**@property {Array<ChargeStation>} chargingStations List of Charging Stations */
     static chargingStations = [];
 
-    /**@property {Array<User>} userList List of registered users */
-    static userList = [];
-
+    static userList = []
     /**
-     * @method 
-     * @name addUser  
      * @param {User} User instance of User
      * @description Method used to add new scooter to list of scooters
+     * @returns {void}
      */
     addUser(User){
         this.userList.push(User);
     }
 
     /**
-     * @method 
-     * @name addScooter
      * @param {Scooter} Scooter instance of Scooter
      * @description  Method used to add new scooter to list of scooters
+     * @returns {void}
      */
     addScooter(Scooter){
         this.allScooters.push(Scooter);
     }
 
     /**
-     * @method
-     * @name 
+     * @param {string} userName
+     * @description Method used to find a user form userList, returns U
+     * @returns {User}
      */
-
-
+    getUser(userName){
+        for (let i = 0; i < userList.length; i++){
+            if (userName == this.userList[i].userList) {
+                return userList[i]
+            }
+        }
+    }
 }
