04c1e209b145335d400c927b572c72b75f44ef51
csontvazharcos <skeletonfighter@gmail.com>
2021-01-15 00:00:35 +0000

    Yeah this makes total sense right

--- classes/h_ScooterHire.js
+++ classes/h_ScooterHire.js
@@ -1,8 +1,3 @@
-//const ScooterHire = require("./h_ScooterHire")
-const ChargingStation = require("./h_ChargingStation");
-const Scooter = require("./h_Scooter");
-const User = require("./h_User");
-const Battery = require("./h_Battery");
 
 /**
  * @class Static Main Object of the Scooter Hire System
@@ -52,4 +47,213 @@ class ScooterHire {
     }
 }
 
-module.exports = ScooterHire
+
+
+/**
+ * @class User object that stores the registered users
+ * @property {string} userName List of every scooter
+ * @property {Scooter|null} availableScooters List of available scooters
+ */
+class User {
+    userName;
+    hiredScooter;
+    
+    /**
+     * @param {string} userName 
+     */
+    constructor(userName){
+        this.userName = userName;
+        ScooterHire.addUser(this);
+    }
+    
+    /**
+     * @description Method to return scooter the user has rented
+     * @returns {void}
+     */
+    returnScooter(){
+
+    }
+
+    /**
+     * @param {Scooter} Scooter Intsance of a scooter to set
+     * @description Method to set rented scooter to the user
+     * @returns {void}
+     */
+    setHiredScooter(Scooter){
+        this.hiredScooter = Scooter
+    }
+
+    /**
+     * @param {string} stationName
+     * @description Method used to get an available scooter at a given station
+     * @returns {Scooter}
+     */
+    getScooterAtStation(stationName){
+        
+    }
+}
+
+
+
+
+
+
+/**
+ * @class ChargingStation object used to store ChargingStations Scooters are brought to
+ * @extends ScooterHire
+ * @property {string} name Name of charging station
+ * @property {number} totalPlaces Number of total places the charging station has
+ * @property {number} usedPlaces Number of places used up by scooters in the charging station
+ * @property {Array<Scooter>} chargingScooters List of the scooters charging at the charging station
+ * @property {Array<Scooter>} chargedScooters List of fully charged scooters at the charging station
+ */
+class ChargingStation {
+    name;
+    totalPlaces;
+    usedPlaces;
+
+    chargingScooters = [];
+    chargedScooters = [];
+
+    /**
+     * 
+     * @param {string} name Name of the charging stations
+     * @param {number} totalPlaces Number of total places the charging station has
+     * @param {number} usedPlaces Number of places used up by scooters in the charging station
+     */
+    constructor(name="NameUndefined", totalPlaces=1){
+        this.name = name;
+        this.totalPlaces = totalPlaces;
+        this.usedPlaces = 0;
+
+        ScooterHire.chargingStations.push(this);
+    }
+
+    /**
+     * @param {Scooter} Scooter instance of Scooter
+     * @description Method used to dock a Scooter to the charging station
+     * @returns {void}
+     */
+    dockScooter(Scooter){
+
+    }
+
+    /**
+     * @param {Scooter} Scooter instance of Scooter
+     * @description Method used to undock a Scooter from the charging station
+     * @returns {void}
+     */
+    undockScooter(Scooter){
+        
+    }
+
+    /**
+     * @description Method used to get the number of free places at the charging station
+     * @returns {number}
+     */
+    getNumberOfFreePlaces(){
+        return this.totalPlaces - this.usedPlaces
+    }
+}
+
+
+
+
+/**
+ * @class Scooter object that stores the scooters at the Scooter Hire
+ * @property {string} serialNum Serial number of the scooter
+ * @property {boolean} isHired state of the scooter if its hired or not
+ * @property {boolean} isCharging state of the scooter if its charging or not
+ * @property {ChargingStation|null} chargingStationAttachedTo the instance of the charging station the scooter is attached to while charging and being docked
+ * @property {Battery} battery the instance of the battery the scooter contains
+ * @property {User|null} hiredBy the istance of the user who hired the scooter
+ */
+class Scooter {
+    serialNum;
+    isHired;
+
+    isCharging;
+    chargingStationAttachedTo;
+
+    battery;
+    hiredBy;
+
+    constructor(){
+        this.isHired = false;
+
+        this.isCharging = false;
+        this.chargingStationAttachedTo = null;
+
+        this.hiredBy = null;
+
+        // Makes sure there is no scooter with the same serial number
+        let tempNum = ScooterHire.allScooters.length + 1 ;
+        for(let i = 0; i < ScooterHire.allScooters.length; i++){
+            if (ScooterHire.allScooters[i].serialNum == ("SN00"+tempNum)){
+                tempNum+=1
+            }
+        }
+        this.serialNum = "SN00"+ tempNum;
+        //this.battery = new Battery();
+
+
+
+        ScooterHire.allScooters.push(this);
+    }
+
+    /**
+     * @param {number} time number of ms to use the scooter for (not the renting time)
+     * @description Method used to use the scooter which drains the battery
+     * @returns {void}
+     */
+    useScooter(time){
+        
+    }
+}
+
+/**
+ * @class Battery object that stores the battery the Scooter is using
+ * @property {string} userName List of every scooter
+ * @property {Scooter|null} availableScooters List of available scooters
+ */
+class Battery {
+    chargeLevel;
+    /**
+     * @param {string} userName 
+     */
+    constructor(){
+        this.chargeLevel = 100
+    }
+    
+    /**
+     * @description Method to charge the battery
+     * @returns {void}
+     */
+    charge(){
+
+    }
+
+    /**
+     * @description Method to check if battery is charged
+     * @returns {boolean}
+     */
+    isCharged(){
+        if (this.chargeLevel == 100){
+            return true
+        }
+        else{
+            return false
+        }
+            
+    }
+
+    /**
+     * @description Method to get the charge level
+     * @returns {number}
+     */
+    getCharge(){
+        return this.chargeLevel   
+    }
+}
+
+module.exports = { ScooterHire, User, ChargingStation, Scooter, Battery }
--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const ChargingStation = require("./classes/h_ChargingStation")
-const ScooterHire = require("./classes/h_ScooterHire")
-const Scooter = require("./classes/h_Scooter.js")
-
-const myHire = new ScooterHire();
-
-console.log(ScooterHire.allScooters.length)
-console.log(ScooterHire.allScooters)
-console.log(ScooterHire)
-// let myChargingStation = new ChargingStation("My First Charing Station",5);
+const h_ScooterHire = require("./classes/h_ScooterHire")
 
 
-let myScooter1 = new Scooter()
-// let myScooter2 = new Scooter()
-// let myScooter3 = new Scooter()
+console.log(h_ScooterHire.ScooterHire.allScooters.length)
+console.log(h_ScooterHire.ScooterHire.allScooters)
+console.log(h_ScooterHire.ScooterHire)
+let myChargingStation = new h_ScooterHire.ChargingStation("My First Charing Station",5);
 
 
-// console.log("Scooter1 " + myScooter1.serialNum)
-// console.log("Scooter2 " + myScooter2.serialNum)
+let myScooter1 = new h_ScooterHire.Scooter()
+let myScooter2 = new h_ScooterHire.Scooter()
+let myScooter3 = new h_ScooterHire.Scooter()
 
-// console.log("Scooter3 " + myScooter3.serialNum)
-// myScooter3.serialNum = "SN004"
-// console.log("Scooter3 after change " + myScooter3.serialNum)
 
-// let myScooter4 = new Scooter()
+console.log("Scooter1 " + myScooter1.serialNum)
+console.log("Scooter2 " + myScooter2.serialNum)
 
-// console.log("Scooter4 " + myScooter4.serialNum + "<-- This should be unique too")
+console.log("Scooter3 " + myScooter3.serialNum)
+myScooter3.serialNum = "SN004"
+console.log("Scooter3 after change " + myScooter3.serialNum)
+
+let myScooter4 = new h_ScooterHire.Scooter()
+
+console.log("Scooter4 " + myScooter4.serialNum + "<-- This should be unique too")
 
 
 
